e89a571890e3a7e98d177f97e57da03a79a76b71
meatball <41243343+hartescout@users.noreply.github.com>
2020-08-02 11:22:15 -0700

    Added variable
    Added user input variable... it's dirt but it works I htink.

--- get_urls.py
+++ get_urls.py
@@ -1,45 +1,47 @@
 from googlesearch import search
 
-advisories_and_vulns_object=open(r"/home/ar/Automating dorks/dorks/advisories_and_vulnerabilities.dorks", "r")
+dork_dir = input ("Enter the full path to dorks directory: ")
+
+advisories_and_vulns_object=open(dork_dir+"/advisories_and_vulnerabilities.dorks", "r")
 advisories_and_vulns_list=advisories_and_vulns_object.readlines()
 
-error_messages_object=open(r"/home/ar/Automating dorks/dorks/error_messages.dorks","r")
+error_messages_object=open(dork_dir+"/error_messages.dorks","r")
 error_messages_list=error_messages_object.readlines()
 
-files_containing_juicy_info_object=open(r"/home/ar/Automating dorks/dorks/files_containing_juicy_info.dorks","r")
+files_containing_juicy_info_object=open(dork_dir+"/files_containing_juicy_info.dorks","r")
 files_containing_juicy_info_list=files_containing_juicy_info_object.readlines()
 
-files_containing_passwords_object=open(r"/home/ar/Automating dorks/dorks/files_containing_passwords.dorks","r")
+files_containing_passwords_object=open(dork_dir+"/files_containing_passwords.dorks","r")
 files_containing_passwords_list=files_containing_passwords_object.readlines()
 
-files_containing_usernames_object=open(r"/home/ar/Automating dorks/dorks/files_containing_usernames.dorks","r")
+files_containing_usernames_object=open(dork_dir+"/files_containing_usernames.dorks","r")
 files_containing_usernames_list=files_containing_usernames_object.readlines()
 
-footholds_object=open(r"/home/ar/Automating dorks/dorks/footholds.dorks","r")
+footholds_object=open(dork_dir+"/footholds.dorks","r")
 footholds_list=footholds_object.readlines()
 
-network_vulnerability_data_object=open(r"/home/ar/Automating dorks/dorks/network_or_vulnerability_data.dorks","r")
+network_vulnerability_data_object=open(dork_dir+"/network_or_vulnerability_data.dorks","r")
 network_vulnerability_data_list=network_vulnerability_data_object.readlines()
 
-pages_containing_login_portals_object=open(r"/home/ar/Automating dorks/dorks/pages_containing_login_portals.dorks","r")
+pages_containing_login_portals_object=open(dork_dir+"/pages_containing_login_portals.dorks","r")
 pages_containing_login_portals_list=pages_containing_login_portals_object.readlines()
 
-sensitive_directories_object=open(r"/home/ar/Automating dorks/dorks/sensitive_directories.dorks","r")
+sensitive_directories_object=open(dork_dir+"/sensitive_directories.dorks","r")
 sensitive_directories_list=sensitive_directories_object.readlines()
 
-sensitive_online_shopping_object=open(r"/home/ar/Automating dorks/dorks/sensitive_online_shopping_info.dorks","r")
+sensitive_online_shopping_object=open(dork_dir+"/sensitive_online_shopping_info.dorks","r")
 sensitive_online_shopping_list=sensitive_online_shopping_object.readlines()
 
-various_online_devices_object=open(r"/home/ar/Automating dorks/dorks/various_online_devices.dorks","r")
+various_online_devices_object=open(dork_dir+"/various_online_devices.dorks","r")
 various_online_devices_list=various_online_devices_object.readlines()
 
-vulnerable_files_object=open(r"/home/ar/Automating dorks/dorks/vulnerable_files.dorks","r")
+vulnerable_files_object=open(dork_dir+"/vulnerable_files.dorks","r")
 vulnerable_files_list=vulnerable_files_object.readlines()
 
-vulnerable_servers_object=open(r"/home/ar/Automating dorks/dorks/vulnerable_servers.dorks","r")
+vulnerable_servers_object=open(dork_dir+"/vulnerable_servers.dorks","r")
 vulnerable_servers_list=vulnerable_servers_object.readlines()
 
-web_server_detection_object=open(r"/home/ar/Automating dorks/dorks/web_server_detection.dorks","r")
+web_server_detection_object=open(dork_dir+"/web_server_detection.dorks","r")
 web_server_detection_list=web_server_detection_object.readlines()
 
 for dork in error_messages_list:
